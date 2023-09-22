0c067a7fb54513658b65097594d7c8067679f852
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-09-22 19:19:21 +0100

    Updated testing

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders all components', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const SpaceInvaders = screen.getByText(/Space Invaders/i);
+  expect(SpaceInvaders).toBeInTheDocument();
 });
