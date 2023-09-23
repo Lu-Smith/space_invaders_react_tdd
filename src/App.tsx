1828cced9028a98df3a7fd642967e3868d431170
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-09-23 19:11:22 +0100

    Updated App.css

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-       Space Invaders
-       <div className="App-shooting-star-1" data-testid="App-shooting-star-1" ></div>
-       <img src={logo} alt="Space Invader Logo" className='App-logo' />
-       <div className="App-shooting-star-2" data-testid="App-shooting-star-2"></div>
-      <button>Start</button>
+        Space Invaders
+        <div className="App-shooting-star-1" data-testid="App-shooting-star-1" ></div>
+        <img src={logo} alt="Space Invader Logo" className='App-logo' />
+        <div className="App-shooting-star-2" data-testid="App-shooting-star-2"></div>
+        <button className='App-start-button'>Start</button>
       </header>
     </div>
   );
