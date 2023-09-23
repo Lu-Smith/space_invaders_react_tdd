2800c4d3fd6dece480074397a699d92fb3cc68d5
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-09-23 22:53:15 +0100

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './media/logo.png';
 import './App.css';
 
 function App() {
+  const [showHeader, setShowHeader] = useState(true);
+
+  const handleClick = () => {
+    setShowHeader(false);
+  }
+
   return (
     <div className="App">
-      <header className="App-header" data-testid="App-header">
+      {showHeader ? (
+        <header className="App-header" data-testid="App-header">
         Space Invaders
         <div className="App-shooting-star-1" data-testid="App-shooting-star-1" ></div>
         <img src={logo} alt="Space Invader Logo" className='App-logo' />
         <div className="App-shooting-star-2" data-testid="App-shooting-star-2"></div>
-        <button className='App-start-button'>Start</button>
+        <button className='App-start-button' onClick={handleClick}>Start</button>
       </header>
+      ) : (
+        <div className="App-game-container">
+          Game Content
+        </div>
+      )}
     </div>
   );
 }
