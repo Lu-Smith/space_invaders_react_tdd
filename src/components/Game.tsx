75a2751679e04b97e052d0eb92677c98de8f3f10
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-09-24 19:54:18 +0100

    Passed test in Game

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Game = () => {
   return (
-    <div data-testid="Game-component">00:00</div>
+    <div data-testid="Game-component">
+      <div className="timer-container" data-testid="timer-container">
+      00:00
+      </div>
+    </div>
   )
 }
 
