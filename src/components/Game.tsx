c189819b0fd1ce0c7c89339530a194363c761429
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-09-24 19:57:53 +0100

    Passed test in Game

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -4,7 +4,10 @@ const Game = () => {
   return (
     <div data-testid="Game-component">
       <div className="timer-container" data-testid="timer-container">
-      00:00
+        00:00
+      </div>
+      <div className="score-container" data-testid="score-container">
+        score
       </div>
     </div>
   )
