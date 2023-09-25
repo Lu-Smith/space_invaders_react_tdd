0101fe7244260c2667567a8be63476c59ec132f0
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-09-25 18:19:34 +0100

    Updated Game.css

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -6,14 +6,14 @@ const Game = () => {
   return (
     <div data-testid="Game-component" className='Game'>
       <header className="Game-header" data-testId="game-header">
-        <div className="timer-container" data-testid="timer-container">
+        <div className="Game-timer-container" data-testid="timer-container">
           00:00
         </div>
-        <div className="score-container" data-testid="score-container">
+        <div className="Game-score-container" data-testid="score-container">
           score
         </div>
       </header>
-      <div className="game-container" data-testid="game-container">
+      <div className="Game-container" data-testid="game-container">
         game
         <div data-testId="game-button-container">pause</div>
       </div>
