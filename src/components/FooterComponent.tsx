beec694c96bb37333f8a403af18a5f6519d27c67
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-09-26 14:24:53 +0100

    Updated Foorter.css

--- src/components/FooterComponent.tsx
+++ src/components/FooterComponent.tsx
@@ -1,20 +1,23 @@
-import React from 'react'
+import React from 'react';
+import "../styles/Footer.css";
 
 const FooterComponent = () => {
   return (
-    <div data-testid="Footer-component">
+    <div data-testid="Footer-component" className='Footer'>
         <h4>This game was coded by 
             <a 
-            className="profile-link" 
+            className="Footer-profile-link" 
             href="https://www.lunasmithart.com/" 
             target="_blank"
-            rel="noreferrer"> Luna Smith </a> 
+            rel="noreferrer"
+            data-text="Luna Smith"> Luna Smith </a> 
             and is available as an open-source project on 
             <a 
-            className="gitHub-link" 
+            className="Footer-gitHub-link" 
             href="https://github.com/Lu-Smith/space_invaders_react_tdd" 
             target="_blank"
-            rel="noreferrer"> GitHub</a>.
+            rel="noreferrer"
+            data-text="GitHub"> GitHub</a>.
         </h4>
     </div>
   )
