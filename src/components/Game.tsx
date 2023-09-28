4a44b2754165eac078b94c958fdb9af6432acfb2
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-09-28 19:41:00 +0100

    Updated Game.tsx

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -5,10 +5,15 @@ import '../styles/Game.css';
 
 const Game = () => {
   const [gameOver, setGameOver] = useState(false);
+  const [score, setScore] = useState(0);
 
   const handleGameOver = () => {
     setGameOver(true);
   };
+
+  const handleScore = () => {
+    setScore(score + 1)
+  }
 
   return (
     <div data-testid="Game-component" className='Game'>
@@ -22,7 +27,7 @@ const Game = () => {
       </header>
       <div className="Game-container" data-testid="game-container">
         {gameOver ? <h3>GameOver</h3> : null}
-        <GameBoard handleGameOver={handleGameOver} />
+        <GameBoard handleGameOver={handleGameOver} handleScore={handleScore} />
         <button className="Game-button" data-testid="game-button">pause</button>
       </div>
       <FooterComponent />
