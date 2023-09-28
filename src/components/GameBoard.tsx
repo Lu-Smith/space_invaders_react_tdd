2396fa5f2387fffeb1f622203ad3bd0b418bf34a
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-09-28 14:57:09 +0100

    Updated logic in GameBoard.tsx

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -10,7 +10,7 @@ const GameBoard: React.FC<GameBoardProps> = ({ handleGameOver }) => {
     const numRows = 20; 
     const numCols = 25; 
     const totalSquares = numRows * numCols;
-    const squares= [];
+    const squares: JSX.Element[]= [];
     const [alienInvaders, setAlienInvaders] = useState([
         0,1,2,3,4,5,6,7,8,9,10,11,12,13,
         26,27,28,29,30,31,32,33,34,35,36,37,
@@ -19,6 +19,8 @@ const GameBoard: React.FC<GameBoardProps> = ({ handleGameOver }) => {
 
     const alienInvadersRemoved = [];
     const [spaceshipIndex, setSpaceshipIndex] = useState(487);
+    const [laserIndex, setLaserIndex] = useState(spaceshipIndex);
+    const [shootLaser, setShootLaser] = useState(false);
     const [direction, setDirection] = useState(1);
 
 
@@ -31,6 +33,10 @@ const GameBoard: React.FC<GameBoardProps> = ({ handleGameOver }) => {
                 if (e.key === 'ArrowRight') {
                     setSpaceshipIndex((prevIndex) => Math.min(prevIndex + 1, totalSquares - 1));
                 }
+                if (e.key === 'ArrowUp') {
+                  setLaserIndex((prevIndex) => Math.min(prevIndex + 25, 0));
+                  setShootLaser(true);
+              }
             }
         };
 
@@ -83,11 +89,11 @@ const GameBoard: React.FC<GameBoardProps> = ({ handleGameOver }) => {
           clearInterval(intervalId);
         };
       }, alienInvaders);
+
     
       for (let i = 0; i < totalSquares; i++) {
         const isInvader = alienInvaders.includes(i);
         const isSpaceship = spaceshipIndex === i;
-    
         const squareClass = isInvader
           ? 'GameBoard-square invader'
           : isSpaceship
